2ef51e2c01bc01e61177b784266884f5f11c0047
Drew <code@drewwestcott.co.uk>
2019-03-26 21:20:38 +0000

    Checks for new files

---
+++ Project 1/Model/Image.swift
@@ -0,0 +1,14 @@
+//
+//  Image.swift
+//  Project 1
+//
+//  Created by Drew Westcott on 22/03/2019.
+//  Copyright © 2019 Drew Westcott. All rights reserved.
+//
+
+import Foundation
+
+struct Image: Codable {
+    var name: String
+    var count: Int
+}
